4ad2ff31708534093208e9cead7a4bd60f8bcb1d
zzl <1364952827@qq.com>
2024-04-15 07:38:35 +0800

    feat: 初始化项目

---
+++ src/plugins/plugin-custom-setter-sample/setters/behavior-setter.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import BehaviorSetter from '@alilc/lowcode-setter-behavior';
+
+
+const defaultExtraBehaviorActions: any[] = [];
+class LocalBehaviorSetter extends React.Component {
+  render() {
+    // ignore url && responseFormatter props, use default ones
+    // @ts-ignore
+    const { url: propsUrl, responseFormatter: propsFormatter, extraBehaviorActions: propsExtraBehaviorActions = [], ...otherProps } = this.props;
+    const url = 'https://hn.algolia.com/api/v1/search?query';
+    const  responseFormatter = (response: { hits: any[]; }) => response.hits.map((item) => ({
+      label: item.title,
+      value: item.author
+    }));
+    const extraBehaviorActions = propsExtraBehaviorActions.concat(defaultExtraBehaviorActions);
+    return (
+      <BehaviorSetter
+        {...otherProps}
+        url={url}
+        responseFormatter={responseFormatter}
+        extraBehaviorActions={extraBehaviorActions}
+      />
+    );
+  }
+}
+
+export default LocalBehaviorSetter;
---
+++ src/plugins/plugin-custom-setter-sample/setters/custom-setter.tsx
@@ -0,0 +1,15 @@
+// @ts-ignore
+import React, { Component } from 'react';
+// import classNames from 'classnames';
+
+class CustomSetter extends Component<any, any> {
+  render() {
+    const {value} = this.props;
+    const { editor } = this.props.field;
+
+    // @ts-ignore
+    return <div>hello world</div>;
+  }
+}
+
+export default CustomSetter;
